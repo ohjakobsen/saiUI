635aff0153ad5b28ffee28fd5d0d7bc807a13577
Ove Haugland Jakobsen <ohj@Oves-MacBook-Pro.local>
2018-04-16 21:35:07 +0200

    Housekeeping

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -84,7 +84,9 @@ saiPage <- function(title,
   tabs[[1]]$attribs$class <- 'tab-pane fade show active'
   # lapply(tabs, function(t) print(t$attribs))
 
-  class = paste0('navbar navbar-expand-lg navbar-dark bg-', color)
+  class = paste0(
+    'navbar navbar-expand-lg ', ifelse(color == 'light', 'navbar-light', 'navbar-dark'), ' bg-', color
+  )
 
   navItems <- buildNavbar(pageTitle, tabs, color)
 
@@ -184,6 +186,7 @@ singleLayout <- function(..., fluid = FALSE, width = 1080) {
 sidebarLayout <- function(menu, main, position = c('left', 'right'), fluid = TRUE) {
   
   # TODO: Add handler for `position`
+  position <- match.arg(position)
   class <- ifelse(fluid, 'container-fluid', 'container')
   
   div(class = class,
@@ -487,41 +490,4 @@ buildTabset <- function(tabs, ulClass, textFilter = NULL,
   # Finally, actually invoke the functions to do the processing.
   tabs <- findAndMarkSelected(tabs, selected)
   build(tabs, ulClass, textFilter, id)
-}
-
-#' Searchbox input
-#'
-#' @param inputId The \code{input} slot that will be used to access the value.
-#' @param value A character string with the default value of the search box
-#' @param placeholder A character string giving the user a hint as to what can be entered into the
-#'   control.
-#' @param button A character string to display as the text for the search button.
-#' @param color A character string giving the color of the search button.
-#' @param size A character string giving the size of the input. Valid options are \code{normal},
-#'   \code{sm} and \code{lg}. Defaults to \code{normal}
-#'
-#' @export
-searchboxInput <- function(inputId, value = '', placeholder = NULL, button = 'Search',
-                           color = c('success', 'primary', 'secondary', 'danger', 'warning', 'light', 'dark'),
-                           size = c('normal', 'sm', 'lg')) {
-
-  # TODO: Make use of the actual button
-  # Adding type = 'submit' cancels all reactive values until the submit button is pressed
-  # We need to find a more elegant solution (add JS-event?)
-  
-  size <- list(
-    form = ifelse(size %in% c('lg', 'sm'), paste0(' form-control-', size), ''),
-    btn = ifelse(size %in% c('lg', 'sm'), paste0(' btn-', size), '')
-  )
-
-  value <- shiny::restoreInput(id = inputId, default = value)
-
-  div(class = 'form-group shiny-input-container',
-    tags$form(class = 'form-inline searchbox px-1 my-2', style = 'width: 100%;',
-      tags$input(id = inputId, type="text", class = paste0('form-control', size$form,' mr-1'),
-                 value = value, placeholder = placeholder),
-      tags$button(id = paste0(inputId, '-btn'), class = paste0('btn btn-outline-', color, size$btn), button)
-      )
-  )
-
 }
--- R/inputs.R
+++ R/inputs.R
@@ -25,6 +25,42 @@ actionButton <- function(inputId, label, color = 'primary', icon = NULL, width =
   )
 }
 
+#' Searchbox input
+#'
+#' @param inputId The \code{input} slot that will be used to access the value.
+#' @param value A character string with the default value of the search box
+#' @param placeholder A character string giving the user a hint as to what can be entered into the
+#'   control.
+#' @param button A character string to display as the text for the search button.
+#' @param color A character string giving the color of the search button.
+#' @param size A character string giving the size of the input. Valid options are \code{normal},
+#'   \code{sm} and \code{lg}. Defaults to \code{normal}
+#'
+#' @export
+searchboxInput <- function(inputId, value = '', placeholder = NULL, button = 'Search',
+                           color = c('success', 'primary', 'secondary', 'danger', 'warning', 'light', 'dark'),
+                           size = c('normal', 'sm', 'lg')) {
+  
+  color <- match.arg(color)
+  size <- match.arg(size)
+  
+  size <- list(
+    form = ifelse(size %in% c('lg', 'sm'), paste0(' form-control-', size), ''),
+    btn = ifelse(size %in% c('lg', 'sm'), paste0(' btn-', size), '')
+  )
+  
+  value <- shiny::restoreInput(id = inputId, default = value)
+  
+  div(class = 'form-group shiny-input-container',
+      tags$form(class = 'form-inline searchbox px-1 my-2', style = 'width: 100%;',
+                tags$input(id = inputId, type = 'text', class = paste0('form-control', size$form, ' mr-1'),
+                           value = value, placeholder = placeholder),
+                tags$button(id = paste0(inputId, '-btn'), class = paste0('btn btn-outline-', color, size$btn), button)
+      )
+  )
+  
+}
+
 dropdownSelect <- function(inputId, label, color = '', icon = NULL, width = NULL, ...,
                            choices = c(), selected = c(), multiple = FALSE) {
   
