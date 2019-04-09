9300c0a38c921f0d398e7abdcc7cab26919d5f55
Ove Haugland Jakobsen <ohj@Oves-RR-MacBook-Pro.local>
2019-04-09 16:38:20 +0200

    Remove on load function (we're only using dependencies)

--- R/onLoad.R
+++
@@ -1,9 +0,0 @@
-#' Adds the content of www to saiUI/
-#'
-#' @importFrom shiny addResourcePath
-#'
-#' @noRd
-#'
-.onLoad <- function(...) {
-  shiny::addResourcePath('saiUI', system.file('www', package = 'saiUI'))
-}
